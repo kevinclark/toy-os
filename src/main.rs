2836cfa0d7d81262d9a7d2b430159b716c486d33
Kevin Clark <kevin.clark@gmail.com>
2020-05-20 13:56:56 -0700

    Add vga buffer

--- src/main.rs
+++ src/main.rs
@@ -3,32 +3,21 @@
 
 use core::panic::PanicInfo;
 
-static OHNO: &[u8] = b"OH NO";
+mod vga_buffer;
+use vga_buffer::Color;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    // Nothing to do for now but
-    // TODO: Print that we panic'd
-    print(OHNO);
+    print!("{}", _info);
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
-
-fn print(s: &[u8]) {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in s.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-}
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    print(HELLO);
+    println!("Hello world{}", "!");
+
+    panic!("foo");
 
     loop {}
 }
